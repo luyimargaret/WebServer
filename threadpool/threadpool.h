b2e8381aafc2b6977f574e1902faa3cba1713dd0
luyimargaret <44019236+luyimargaret@users.noreply.github.com>
2022-07-21 18:10:06 +0800

    Update threadpool.h

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -1,130 +1,116 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
-//半同步/半反应堆线程池
 #include <list>
 #include <cstdio>
 #include <exception>
 #include <pthread.h>
-#include "locker.h"
+#include "../lock/locker.h"
+#include "../CGImysql/sql_connection_pool.h"
 
-//线程池类，将它定义为模板类是为了代码复用，模板参数T是任务类
-template< typename T >
+template <typename T>
 class threadpool
 {
 public:
-    //thread_number:线程池中线程的数量 max_requests:请求队列中最多允许的、等待处理的请求数量
-    threadpool( int thread_number = 8, int max_requests = 10000 );
+    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    threadpool(connection_pool *connPool, int thread_number = 8, int max_request = 10000);
     ~threadpool();
-     //往请求队列中添加任务
-    bool append( T* request );
-   
+    bool append(T *request);
+
 private:
-    //工作线程运行的函数，不断从工作队列中取出任务并执行
-    static void* worker( void* arg );
+    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
+    static void *worker(void *arg);
     void run();
 
 private:
-    int m_thread_number;      //线程池中的线程数
-    int m_max_requests;        //请求队列中允许的最大请求数
-    pthread_t* m_threads;      //描述线程池的数组，大小为thread_number
-    std::list< T* > m_workqueue; //请求队列
-    locker m_queuelocker;          //保护请求队列的互斥锁
-    sem m_queuestat;           //是否有任务需要处理
-    bool m_stop;             //是否结束线程
+    int m_thread_number;        //线程池中的线程数
+    int m_max_requests;         //请求队列中允许的最大请求数
+    pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
+    std::list<T *> m_workqueue; //请求队列
+    locker m_queuelocker;       //保护请求队列的互斥锁
+    sem m_queuestat;            //是否有任务需要处理
+    bool m_stop;                //是否结束线程
+    connection_pool *m_connPool;  //数据库
 };
-
-template< typename T >
-threadpool< T >::threadpool( int thread_number, int max_requests ) : 
-        m_thread_number( thread_number ), m_max_requests( max_requests ), m_stop( false ), m_threads( NULL )
+template <typename T>
+threadpool<T>::threadpool( connection_pool *connPool, int thread_number, int max_requests) : m_thread_number(thread_number), m_max_requests(max_requests), m_stop(false), m_threads(NULL),m_connPool(connPool)
 {
-    if( ( thread_number <= 0 ) || ( max_requests <= 0 ) )
-    {
+    if (thread_number <= 0 || max_requests <= 0)
         throw std::exception();
-    }
-    m_threads = new pthread_t[ m_thread_number ];
-    if( ! m_threads )
-    {
+    m_threads = new pthread_t[m_thread_number];
+    if (!m_threads)
         throw std::exception();
-    }
     //创建thread_number个线程，将他们都设置为脱离线程
-    for ( int i = 0; i < thread_number; ++i )
+    for (int i = 0; i < thread_number; ++i)
     {
+        //printf("create the %dth thread\n",i);
         //pthread_create（）第三个参数要求为静态函数
         //循环创建线程，并将工作线程按要求进行运行
-        if( pthread_create( m_threads + i, NULL, worker, this ) != 0 )
+        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
         {
-            delete [] m_threads;
+            delete[] m_threads;
             throw std::exception();
         }
-        if( pthread_detach( m_threads[i] ) )
+        if (pthread_detach(m_threads[i]))
         {
-            delete [] m_threads;
+            delete[] m_threads;
             throw std::exception();
         }
     }
 }
-
-template< typename T >
-threadpool< T >::~threadpool()
+template <typename T>
+threadpool<T>::~threadpool()
 {
-    delete [] m_threads;
+    delete[] m_threads;
     m_stop = true;
 }
-
-template< typename T >
-bool threadpool< T >::append( T* request )
+template <typename T>
+bool threadpool<T>::append(T *request)
 {
-    //操作工作队列时一定要加锁，因为他被所有线程共享
-    m_queuelocker.lock();·
-    if ( m_workqueue.size() > m_max_requests )
+    m_queuelocker.lock();
+    if (m_workqueue.size() > m_max_requests)
     {
         m_queuelocker.unlock();
         return false;
     }
-    //添加任务
-    m_workqueue.push_back( request );
+    m_workqueue.push_back(request);
     m_queuelocker.unlock();
-    //信号量提醒有任务要处理
     m_queuestat.post();
     return true;
 }
-
-template< typename T >
-void* threadpool< T >::worker( void* arg )
+template <typename T>
+void *threadpool<T>::worker(void *arg)
 {
-    //将参数强制转为线程池类，调用成员方法
-    threadpool* pool = ( threadpool* )arg;
+    threadpool *pool = (threadpool *)arg;
     pool->run();
     return pool;
 }
 //工作线程从请求队列中取出某个任务进行处理
-template< typename T >
-void threadpool< T >::run()
+template <typename T>
+void threadpool<T>::run()
 {
-    while ( ! m_stop )
+    while (!m_stop)
     {
         //信号量等待
         m_queuestat.wait();
         //被唤醒后先加互斥锁
         m_queuelocker.lock();
-        if ( m_workqueue.empty() )
+        if (m_workqueue.empty())
         {
             m_queuelocker.unlock();
             continue;
         }
         //请求队列中取出第一个任务
         //将任务从队列中删除
-        T* request = m_workqueue.front();
+        T *request = m_workqueue.front();
         m_workqueue.pop_front();
         m_queuelocker.unlock();
-        if ( ! request )
-        {
+        if (!request)
             continue;
-        }
+
+        connectionRAII mysqlcon(&request->mysql, m_connPool);
         //process(模板类中的方法，这里是http类)进行处理
         request->process();
     }
 }
-
 #endif
